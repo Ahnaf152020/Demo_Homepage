72fa2d34a30b3d1d3ddcacda66be91b4908a1a30
Ahnaf <ahnafamer15@gmail.com>
2024-06-09 00:35:03 +0600

    Changes in search page

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import CardMedia from '@mui/material/CardMedia';
 
 const SearchPage = () => {
   const [data, setData] = useState([]);
@@ -27,7 +29,7 @@ const SearchPage = () => {
   }, [query, data]);
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Typography variant="h4" component="h1" gutterBottom>
         Search
       </Typography>
@@ -45,17 +47,34 @@ const SearchPage = () => {
           ),
         }}
       />
-      {filteredData.length === 0 ? (
-        <Typography variant="h6" component="p" gutterBottom>
-          No results found
-        </Typography>
-      ) : (
-        <List>
-          {filteredData.map(item => (
-            <ListItem key={item.id}>{item.name}</ListItem>
-          ))}
-        </List>
-      )}
+      <Grid container spacing={2}>
+        {filteredData.length > 0 ? (
+          filteredData.map(item => (
+            <Grid item xs={12} sm={6} md={4} key={item.id}>
+              <Card>
+              <CardMedia
+  component="img"
+  height="140"
+  image={item.imageUrl} // Change this to 'imageUrl'
+  alt={item.name}
+/>
+
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.name}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Grid item xs={12}>
+            <Typography variant="body1" component="p">
+              No results found.
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
     </Container>
   );
 };
